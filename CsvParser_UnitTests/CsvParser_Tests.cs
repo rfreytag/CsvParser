455d0d03b1c4dc1c2765db009d09961979ef35f1
Richard Freytag <rfreytag@freytag.us>
2020-07-29 19:16:35 -0400

    Add unit test demonstrating ICsvReaderRow.TakeLast<string>(...).

--- CsvParser_UnitTests/CsvParser_Tests.cs
+++ CsvParser_UnitTests/CsvParser_Tests.cs
@@ -2,6 +2,7 @@ using CsvParser;
 using NUnit.Framework;
 using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Tests
@@ -105,6 +106,66 @@ namespace Tests
 
             Assert.True(line2.Count > 0);
             Assert.True(line1.Count > 0);  // because call-by-reference;
+
+            try
+            {
+                ele1 = line1[1]; // should throw an error
+            }
+            catch (Exception ex2)
+            {
+                call_by_reference_exception = true;
+            }
+
+            Assert.False(call_by_reference_exception);
+        }
+
+        [Test]
+        public void LineCurrent_TakeLast_Test()
+        {
+            var no_basic_exceptions = true;
+            var call_by_reference_exception = false;
+
+            ICsvReaderRow line1 = null;
+            ICsvReaderRow line2 = null;
+            ICsvReaderRow line3 = null;
+            string ele1 = string.Empty;
+            string ele2 = string.Empty;
+            string ele_2ndOfLast4 = String.Empty;
+
+            try
+            {
+                var csvreader = new CsvParser.CsvReader(new MemoryStream(Encoding.UTF8.GetBytes(content)),
+                                                                         Encoding.UTF8);
+
+                csvreader.MoveNext();
+                line1 = csvreader.CurrentByValue;
+                ele1 = line1[1];
+
+
+                csvreader.MoveNext();
+                line2 = csvreader.CurrentByValue;
+                ele2 = line2[1];
+
+                csvreader.MoveNext();
+                line3 = csvreader.CurrentByValue;
+                ele_2ndOfLast4 = line3.TakeLast(4).ToList()[1];
+
+
+            }
+            catch (Exception ex1)
+            {
+                no_basic_exceptions = false;
+            }
+
+            Assert.True(no_basic_exceptions);
+
+            Assert.AreEqual("A2 (ISO)", ele1);
+            Assert.AreEqual("AF", ele2);
+            Assert.AreEqual("355", ele_2ndOfLast4);
+
+            Assert.True(line1.Count > 0);  // because call-by-reference;
+            Assert.True(line2.Count > 0);
+            Assert.True(line3.Count > 0);
 
             try
             {
